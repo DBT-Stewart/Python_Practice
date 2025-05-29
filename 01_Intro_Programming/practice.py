718042cce0bd9aa9617703172b435cc294e6b26f
DBT-Stewart <stewartprince.PM@diggibyte.com>
2025-05-29 12:33:43 +0530

    C1 - Python file created and made some practice

---
+++ 01_Intro_Programming/practice.py
@@ -0,0 +1,19 @@
+print("Hi Stewart, welcome to Python programming!")
+a=10
+b=20
+c=a+b
+print("The sum of", a, "and", b, "is", c)
+
+print(type(a))  # Output: <class 'int'>
+print(type(b))  # Output: <class 'int'>
+print(type(c))  # Output: <class 'int'>
+
+d="Hello, Stewart!"
+print(type(d))  # Output: <class 'str'>
+print("The length of the string is:", len(d))  # Output: The length of the string is: 15
+
+e=int(input("Enter a number: "))
+print("You entered:", e)
+
+f=int(input("Enter another number: "))
+print("The sum of the two numbers is:", e + f)
