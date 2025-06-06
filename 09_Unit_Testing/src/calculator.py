f0e5f0949f2b4ad16168b524fafaa5bfa6627628
DBT-Stewart <stewartprince.PM@diggibyte.com>
2025-06-06 06:49:42 +0530

    C23.2 - Unit Testing Code (with error)

--- 09_Unit_Testing/src/calculator.py
+++ 09_Unit_Testing/src/calculator.py
@@ -7,9 +7,9 @@ def subtract(x, y):
     return x - y
 
 def multiply(x, y):
-    return x * y
+    return x / y
 
 def divide(x, y):
     if y == 0:
         raise ValueError("Cannot divide by zero")
-    return x / y
+    return x % y
